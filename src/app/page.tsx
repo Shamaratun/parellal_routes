a31808f3160ca2235329205c400eabfa28eeb400
Shamaratun <shamaratunmeme@gmail.com>
2025-10-28 18:36:22 +0600

    design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import PortalModal from "@/components/utility-components/portal-modal";
 import GetPatientInfoWithId from "./dashboard/patient-profile/getPatientInfoWithId";
+import MultiStepForm from "./dashboard/uploadTesting/page";
 
 
 export default function DashboardPage() {
@@ -75,6 +76,7 @@ export default function DashboardPage() {
           <GetPatientInfoWithId patient_id={patientId} admission_id={admissionId} />
         </PortalModal>
       )}
+      <MultiStepForm/>
     </div>
   );
 }
